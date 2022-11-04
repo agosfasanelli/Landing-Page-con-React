4648b1dbb633f10f47137870c013d69d02684dd5
agosfasanelli <agosfasanelli@gmail.com>
2022-11-04 16:01:54 +0000

    hshshs

---
+++ src/js/component/Jumbotron.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Jumbotron = () => {
+    return
+}
+
+export default Jumbotron
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Navbar from "./navbar";
+import Card from "./card";
+import Jumbotron from "./Jumbotron";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,20 +9,14 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
+		<>
+		<Navbar />
+		<Jumbotron />
+		<Card />
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Hola Mundo</h1>
 		</div>
+		</>
 	);
 };
 
