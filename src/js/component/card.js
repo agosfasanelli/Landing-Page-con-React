18f6a33d551671ad83861eec38973c7f48b7d91f
agosfasanelli <agosfasanelli@gmail.com>
2022-11-18 22:56:36 +0000

    hbcdsjah

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -6,41 +6,56 @@ const Card = () => {
   }
 
   return( 
-    <div className="col-9 d-flex mx-auto">
-      <div className="card ms-1 me-1" style={image_style}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
-          <div className="card-body text-center">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat egestas lectus, bibendum commodo nisl imperdiet a.</p>
-            <a href="#" className="btn btn-primary mb-2 mt-5">Find Out More!</a>
+        <div className="row row-cols-1 row-cols-md-4">
+          <div className="col mb-3">
+            <div className="card h-100 card text-center">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              </div>
+              <div className="card-footer">
+                <button type="button" class="btn btn-primary">Find Out More!</button>
+              </div>
+            </div>
           </div>
-      </div>
-      <div className="card ms-1 me-1" style={image_style}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
-          <div className="card-body text-center">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat egestas lectus, bibendum commodo nisl imperdiet a. Etiam interdum, ex sit amet ultricies hendrerit, augue urna dictum turpis, ac consectetur sem justo et arcu. Praesent a auctor nibh.</p>
-            <a href="#" className="btn btn-primary">Find Out More!</a>
+          <div className="col mb-3">
+            <div className="card h-100 card text-center">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              </div>
+              <div className="card-footer">
+                <button type="button" class="btn btn-primary">Find Out More!</button>
+              </div>
+            </div>
           </div>
-      </div>
-      <div className="card ms-1 me-1" style={image_style}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
-          <div className="card-body text-center">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat egestas lectus, bibendum commodo nisl imperdiet a.</p>
-            <a href="#" className="btn btn-primary">Find Out More!</a>
+          <div className="col mb-3">
+            <div className="card h-100 card text-center">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              </div>
+              <div className="card-footer">
+                <button type="button" class="btn btn-primary text-center">Find Out More!</button>
+              </div>
+            </div>
           </div>
-      </div>
-      <div className="card ms-1 me-1" style={image_style}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
-          <div className="card-body text-center">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat egestas lectus, bibendum commodo nisl imperdiet a. Etiam interdum, ex sit amet ultricies hendrerit, augue urna dictum turpis, ac consectetur sem justo et arcu. Praesent a auctor nibh</p>
-            <a href="#" className="btn btn-primary ">Find Out More!</a>
+          <div className="col mb-3">
+            <div className="card h-100 card text-center">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDI4rgxbjoRk5v_RufxNCOWZj5PbWC2f4BGY9XC_x6&s" className="card-img-top" alt="..."/>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+              </div>
+              <div className="card-footer">
+                <button type="button" className="btn btn-primary">Find Out More!</button>
+              </div>
+            </div>
           </div>
-      </div>
-    </div>
-      
-      
+        </div>
+  
 )}
 export default Card
